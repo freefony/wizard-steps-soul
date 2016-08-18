3d14eaf371d79f9c1c8a068a716f3954088b9ec1
Femi Oni <freefony@gmail.com>
2016-08-18 15:22:13 +0100

    feat: set current wizard scope

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 var noop;
+String.prototype.toCamelCase = function () {
+    return this
+        .replace(/\s(.)/g, function ($1) { return $1.toUpperCase(); })
+        .replace(/\s/g, '')
+        .replace(/^(.)/, function ($1) { return $1.toLowerCase(); });
+};
 var wizard = function () {
     var _options;
     var iteratorPointer;
@@ -8,7 +14,40 @@ var wizard = function () {
     var _afterExit;
     var stepLength;
     var _eventRegistry = {};
-    var _eventDispatcher = function (event, eventValue) {
+    var create = function (options) {
+        _options = options;
+        entryStep = options.entryStepNumber ? options.entryStepNumber : 1;
+        iteratorPointer = entryStep;
+        _beforeExit = options.beforeExit ? options.beforeExit : noop;
+        _afterExit = options.afterExit ? options.afterExit : noop;
+        stepLength = options.steps.length;
+    };
+    var _iteratorTicker = {
+        next: function () { iteratorPointer++; },
+        prev: function () { iteratorPointer--; },
+        go: function (index) { iteratorPointer = index; }
+    };
+    var _setScope = function () {
+        var _currentStep = _options.steps[iteratorPointer];
+        var scope = _currentStep
+            .actions
+            .reduce(function (scope, action) {
+            var key = action.label.toCamelCase();
+            scope[key] = action;
+            return scope;
+        }, {});
+        scope.templateUrl = _currentStep.templateUrl;
+        _eventDispatch('onScopeChange', scope);
+        return scope;
+    };
+    var next = function () {
+        _iteratorTicker.next();
+        var currentStep = _options.steps[iteratorPointer];
+        currentStep._beforeNext();
+        _setScope();
+        currentStep._afterNext();
+    };
+    var _eventDispatch = function (event, eventValue) {
         if (eventValue === void 0) { eventValue = {}; }
         if (!_eventRegistry.hasOwnProperty(event)) {
             return;
@@ -19,14 +58,6 @@ var wizard = function () {
         catch (e) {
             console.error('Error: invalid callback function supplied for wizard event' + event);
         }
-    };
-    var create = function (options) {
-        _options = options;
-        entryStep = options.entryStepNumber ? options.entryStepNumber : 1;
-        iteratorPointer = entryStep;
-        _beforeExit = options.beforeExit ? options.beforeExit : noop;
-        _afterExit = options.afterExit ? options.afterExit : noop;
-        stepLength = options.steps.length;
     };
     var $on = function (event, callback) {
         _eventRegistry[event] = callback;
